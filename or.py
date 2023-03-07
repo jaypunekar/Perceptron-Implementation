a4beca09876eb0a3ab98096c769dfffa1b9245db
Jay Punekar <jaypunekar01@gmail.com>
2023-03-07 15:49:41 +0530

    or model created

---
+++ or.py
@@ -0,0 +1,27 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_plot
+import pandas as pd
+
+def main(data, modelName, plotName, eta, epochs):
+    df_OR = pd.DataFrame(OR)
+    X, y = prepare_data(df_OR)
+
+    model_or = Perceptron(eta=eta, epochs=epochs)
+    model_or.fit(X, y)
+
+    _ = model_or.total_loss()
+
+    model_or.save(filename="or.model", model_dir="model_or")
+
+    save_plot(df_OR, model_or, filename="or.png")
+
+if __name__ == "__main__":
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y" : [0,1,1,1]
+    }
+    ETA = 0.3
+    EPOCHS = 10
+    main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+
