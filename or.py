700c6d75127fa54c2ac0c5a7512bd6a755d51636
Jay Punekar <jaypunekar01@gmail.com>
2023-03-09 03:06:47 +0530

    logging added for OR model

--- or.py
+++ or.py
@@ -1,9 +1,23 @@
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_plot
 import pandas as pd
+import logging
+import os
+
+gate = "OR"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join(log_dir, "running_logs.log"),
+    level= logging.INFO,
+    format='[%(asctime)s: %(levelname)s: %(module)s]: %(message)s',
+    filemode='a'
+    )
 
 def main(data, modelName, plotName, eta, epochs):
     df_OR = pd.DataFrame(OR)
+    logging.info(f"This is the raw dataset: {df_OR}")
     X, y = prepare_data(df_OR)
 
     model_or = Perceptron(eta=eta, epochs=epochs)
@@ -23,5 +37,11 @@ if __name__ == "__main__":
     }
     ETA = 0.3
     EPOCHS = 10
-    main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+    try:
+        logging.info(f">>>>> Starting Training for {gate} >>>>>")
+        main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+        logging.info("<<<<< Done training <<<<<")
+    except Exception as e:
+        logging.exception(e)
+        raise e
 
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -2,13 +2,14 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
+import logging
 
 plt.style.use("fivethirtyeight") ## << optional step
 
 
 def prepare_data(df, target_col="y"):
+    logging.info("Preparing data for training")
     X = df.drop(target_col, axis=1)
-    
     y = df[target_col]
     
     return X, y 
@@ -16,6 +17,7 @@ def prepare_data(df, target_col="y"):
 
 def save_plot(df, model, filename="plot.png", plot_dir="plots"):
     def _create_base_plot(df):
+        logging.info("Creating base plot")
         df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="coolwarm")
         plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
         plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -24,6 +26,7 @@ def save_plot(df, model, filename="plot.png", plot_dir="plots"):
         figure.set_size_inches(10, 8)
     
     def _plot_decision_regions(X, y, classifier, resolution=0.02):
+        logging.info("Plotting the decision regions")
         colors = ("cyan", "lightgreen")
         cmap = ListedColormap(colors)
         
@@ -54,3 +57,4 @@ def save_plot(df, model, filename="plot.png", plot_dir="plots"):
     os.makedirs(plot_dir, exist_ok=True)
     plot_path = os.path.join(plot_dir, filename)
     plt.savefig(plot_path)
+    logging.info(f"Saving the plot at {plot_path}")
