7c6c00566069280c39fa8759a4ea66c20fa1b159
Jay Punekar <jaypunekar01@gmail.com>
2023-03-09 03:17:15 +0530

    AND model logging added and Docstring added

--- and.py
+++ and.py
@@ -1,9 +1,12 @@
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_plot
 import pandas as pd
+import logging
+import os
 
 def main(data, modelName, plotName, eta, epochs):
     df_AND = pd.DataFrame(AND)
+    logging.info(f"The is the raw dataset: {df_AND}")
     X, y = prepare_data(df_AND)
 
     model_and = Perceptron(eta=eta, epochs=epochs)
@@ -23,5 +26,13 @@ if __name__ == "__main__":
     }
     ETA = 0.3
     EPOCHS = 10
-    main(data=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
+
+    try:
+        logging.info(f">>>>> Starting Training for {gate} >>>>>")
+        main(data=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
+        logging.info("<<<<< Done training <<<<<")
+    except Exception as e:
+        logging.exception(e)
+        raise e
+
 
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -8,6 +8,15 @@ plt.style.use("fivethirtyeight") ## << optional step
 
 
 def prepare_data(df, target_col="y"):
+    """It returns label and independent features
+
+    Args:
+        df (pd.DataFrame): This is a DataFrame
+        target_col (str, optional): label col name. Defaults to "y".
+
+    Returns:
+        tuple: label and X
+    """
     logging.info("Preparing data for training")
     X = df.drop(target_col, axis=1)
     y = df[target_col]
